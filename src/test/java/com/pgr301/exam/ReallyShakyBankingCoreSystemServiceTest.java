9d82ad54dc3c63117bdc2db89cb7808a59d6057b
Håkon Nikolai Haagaas <hakonnikolaihaagaas@Hakons-MacBook-Pro.local>
2021-11-25 12:58:17 +0100

    test

---
+++ src/test/java/com/pgr301/exam/ReallyShakyBankingCoreSystemServiceTest.java
@@ -0,0 +1,45 @@
+package com.pgr301.exam;
+
+import com.pgr301.exam.model.Account;
+import com.pgr301.exam.model.Transaction;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReallyShakyBankingCoreSystemServiceTest {
+
+    ReallyShakyBankingCoreSystemService shaky = new ReallyShakyBankingCoreSystemService();
+
+    @Test
+    void transfer() {
+        Transaction tx = new Transaction();
+        Account from = shaky.getAccount("123");
+        Account to = shaky.getAccount("456");
+        shaky.transfer(tx, "123", "456");
+        //Uferdig kode
+    }
+
+    @Test
+    void updateAccount() {
+
+    }
+
+    @Test
+    void balance() {
+        Account a = shaky.getAccount("123");
+        BigDecimal balance = new BigDecimal(100);
+        BigDecimal expected = new BigDecimal(100);
+        a.setBalance(balance);
+
+        assertEquals(a.getBalance(), expected);
+    }
+
+    @Test
+    void getAccount() {
+        String expectedNum = "99345";
+        String actualNum = shaky.getAccount("99345").toString();
+        assertEquals(expectedNum, actualNum);
+    }
+}
